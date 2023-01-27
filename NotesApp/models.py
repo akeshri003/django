a50216b89cb4f53aa4ec1f3cab6cd87ccb171708
akeshri003 <101141783+akeshri003@users.noreply.github.com>
2023-01-27 23:46:35 +0530

    correction1: add super().save() method in models

--- NotesApp/models.py
+++ NotesApp/models.py
@@ -7,6 +7,9 @@ class Note(models.Model):
     heading = models.CharField(max_length=100, blank=True, default='Insert Heading')
     content = models.CharField(max_length=1000, blank=True, default='Write your Note...')
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
     class Meta:
         ordering = ["created"]
 
