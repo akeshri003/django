abdf3cee8920700ffd307f157009e54c25b35730
akeshri003 <101141783+akeshri003@users.noreply.github.com>
2023-01-27 23:56:21 +0530

    corrected final

--- NotesApp/models.py
+++ NotesApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Note(models.Model):
-    owners = models.ForeignKey('auth.User', related_name='notes', on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User', related_name='notes', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     heading = models.CharField(max_length=100, blank=True, default='Insert Heading')
     content = models.CharField(max_length=1000, blank=True, default='Write your Note...')
